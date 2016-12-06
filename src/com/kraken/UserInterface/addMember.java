e3ccc12cefb36022f69c5a3d8fe6855d355e8afd
Jessica <jessiebeark@gmail.com>
2016-12-05 22:55:12 -0600

    adding addMember

---
+++ src/com/kraken/UserInterface/addMember.java
@@ -0,0 +1,28 @@
+package com.kraken.UserInterface;
+
+import com.kraken.DataStructures.Members.Member;
+
+import javax.swing.*;
+
+/**
+ * Created by Jessica on 12/5/16.
+ */
+public class addMember {
+    private JTextField nameTextField;
+    private JComboBox<String> comboBox1;
+    private JTextField passwordTextField;
+
+    public Member createNewObject() {
+        comboBox1.insertItemAt("Librarian", 1);
+        comboBox1.insertItemAt("Patron", 2);
+        Member newMember = new Member();
+        newMember.setName(nameTextField.getText());
+        newMember.setPassword(passwordTextField.getText());
+        if (comboBox1.getSelectedItem().equals("Librarian")) {
+            newMember.setLibrarian(true);
+        }
+        newMember.setCanCheckOut(true);
+        return newMember;
+    };
+
+}
